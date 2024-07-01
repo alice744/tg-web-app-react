8190de72176cb7cd7ba604b1791f97d547e9ca53
alice <alice.winner74@gmail.com>
2024-07-01 21:34:53 +0700

    fix memo again

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -32,7 +32,7 @@ const Form = () => {
         return () => {
             tg.offEvent('mainButtonClicked', onSendData);
         }
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
